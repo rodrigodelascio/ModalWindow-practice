555cde9ff1b0177d7813cc2ee522e580033476fe
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-03-11 17:23:47 +0000

    modal popup and overlay functionality added

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) console.log(btnsOpenModal[i].textContent);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+
+    btnsOpenModal[i].addEventListener("click", function () {
+
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+
+    });
+};
