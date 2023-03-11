9d5262829df99b9c42ec7f8199b4c646c414e899
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-03-11 17:42:35 +0000

    modal functionality completely implemented

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-
-    btnsOpenModal[i].addEventListener("click", function () {
-
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-
-    });
+function openModel() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 };
+
+function closeModel() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModel);
+};
+
+btnCloseModal.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        closeModel();
+    };
+});
